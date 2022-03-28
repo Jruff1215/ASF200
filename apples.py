b2bdced0f6776b3a89351aa4ef362f974f8e2934
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2022-03-27 20:59:04 -0400

    first commit

---
+++ apples.py
@@ -0,0 +1,12 @@
+applePrice = "$0.50"
+
+name_insert = "Please enter your name:"
+name_output = "John"
+appleAmount = "6"
+message_1 = f"Hi {name_output.title()}. Apples cost {applePrice} each. How many would you like to buy?"
+message_2 = f"Thank you {name_output.title()} for your purchase of {appleAmount} apples at a cost of {applePrice} each."
+print(name_insert)
+print(name_output)
+print(message_1,)
+print(appleAmount)
+print(message_2)

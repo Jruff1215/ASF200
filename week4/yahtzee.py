a7e0f75366c5c00e52a2cd5ac8b558804e03bc70
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2022-04-17 20:28:49 -0400

    yahtzee

---
+++ week4/yahtzee.py
@@ -0,0 +1,33 @@
+import random
+
+class Die:
+   def __init__(self, sides):
+       self.sides = sides
+       self.value = 0
+   def roll(self):
+       self.value = random.randrange(1, self.sides)
+   
+   def getCurrentFaceValue(self):
+       return self.value
+  
+   def showDieFace(self):
+       print(self.value)
+Die1 = Die(100)
+Die2 = Die(100)
+Die3 = Die(100)
+Die4 = Die(100)
+Die5 = Die(100)
+Die1.roll()
+Die2.roll()
+Die3.roll()
+Die4.roll()
+Die5.roll()
+DieValue1 = Die1.getCurrentFaceValue()
+DieValue2 = Die2.getCurrentFaceValue()
+DieValue3 = Die3.getCurrentFaceValue()
+DieValue4 = Die4.getCurrentFaceValue()
+DieValue5 = Die5.getCurrentFaceValue()
+
+print(f'({DieValue1}) ({DieValue2}) ({DieValue3}) ({DieValue4}) ({DieValue5}) ')
+if DieValue1 == DieValue2 and DieValue2 == DieValue3 and DieValue3 == DieValue4 and DieValue4 == DieValue5:
+    print("YAHTZEE!!!")
